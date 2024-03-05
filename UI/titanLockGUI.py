9601a3094f143a85b65050272e5de5a6ee5cc0c6
Timothy Tran <timmyster413@csu.fullerton.edu>
2024-03-04 18:29:46 -0800

    tim

--- UI/titanLockGUI.py
+++ UI/titanLockGUI.py
@@ -4,12 +4,16 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 class Titan_Locker(tk.Tk):
 
+    def userInputTxtFile():
+        var1 = 2 #Function should allow user input to do something
+        
     def __init__(self):
         super().__init__()
         
         # GUI Dimensions
-        self.title("Titan Lock")
+        self.title("Titan Lock 🐘")
         self.geometry("400x400+500+400")
+        self.minsize(500,420)
         self.resizable(True, True)
         
         # GUI Layout
@@ -21,14 +25,13 @@ class Titan_Locker(tk.Tk):
         self.configure(bg=t_blue) 
         text = tk.Label(
             self, 
-            text='Titan Lock', 
+            text='Titan Lock 🐘', 
             font='Arial 36', 
             bg=t_orange, 
             fg=t_blue, 
             pady=10,
             padx=60
             )
-        #padx was 80
         
         def save_file():
             """Save the current file as a new file."""
@@ -36,13 +39,16 @@ class Titan_Locker(tk.Tk):
                 defaultextension=".txt",
                 filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
             )
+            
             if not filepath:
                 return
+            
             with open(filepath, mode="w", encoding="utf-8") as output_file:
-                userText = userEntry.get()#txt_edit.get("1.0", tk.END)
+                userText = userEntry.get() #txt_edit.get("1.0", tk.END)
                 userPW = pwEntry.get() #currently spaces are allowed
                 link = linkEntry.get()
                 output_file.write(userText  +' '+ userPW  +' '+ link)
+                
             self.title(f"Simple Text Editor - {filepath}")
         
         # Title pack
@@ -56,15 +62,26 @@ class Titan_Locker(tk.Tk):
         pwEntry = Entry(width ='25')
         link = tk.Label(self, text="Link: ", bg= t_blue, fg='white')
         linkEntry = Entry(width ='25')
-        # Set the position of button (PACK)
-        user.pack(pady=(30,5), padx = (0,117))
+        
+        # Set the position of button 
+        user.pack(pady=(30,5), padx = (0,160))
         userEntry.pack()
-        pw.pack(pady=(30,5), padx = (0,91))
+        
+        pw.pack(pady=(30,5), padx = (0,133))
+        # Show asterisks (*) for password entry
+        pwEntry = Entry(width ='25', show='*') 
         pwEntry.pack()
-        link.pack(pady=(30,5), padx=(0,119))
+        
+        link.pack(pady=(30,5), padx=(0,165))
         linkEntry.pack()
+
         btn.pack(pady=(40,10))
         
+    #create a command for the submit button to take the user entry 
+    def userInput(): # Should take the input from the entry then send it to a file via thesubmitbutto
+        var2 = 5
+
+
 if __name__ == "__main__":
     app = Titan_Locker()
     app.mainloop()
