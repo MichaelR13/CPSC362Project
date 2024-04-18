0382603315ff0c163303468bfa17a8e928a5849c
unknown <kchan21@csu.fullerton.edu>
2024-04-17 18:38:05 -0700

    fixed decryption cuz im stupid

--- Cryptography/decryption.py
+++ Cryptography/decryption.py
@@ -20,7 +20,7 @@ def decrypt(key):
 #delim into a list and pass the list into decrypt to get csv file
 def delimiter(line):
     splitWords = line.split('|')
-    splitWords.pop()
+    splitWords.pop(0)
     return splitWords
     
 def decode(lst, key):
@@ -34,17 +34,13 @@ def decode(lst, key):
     for item in lst:
         try:
             # Convert the string representation of bytes back to bytes
-            encrypted_data = eval(item)
-
+            encrypted_data = eval(item) 
             # Set up the AES decryption cipher
             dec_cipher = AES.new(key, AES.MODE_ECB)
-
             # Decrypt the data
             decrypted_data = dec_cipher.decrypt(encrypted_data)
-
             # Remove padding from the decrypted data
             unpadded_data = unpad(decrypted_data, AES.block_size)
-
             final_list.append(unpadded_data.decode('utf-8'))    # Convert the bytes back to a string
         except Exception as e:
             print("Error decrypting data:", e)
