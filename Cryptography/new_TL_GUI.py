96562a0e3331830f51e6eec650e01d939358cc4b
Timothy Tran <timmyster413@csu.fullerton.edu>
2024-04-24 18:20:35 -0700

    modded new_tl_gui -tim

--- Cryptography/new_TL_GUI.py
+++ Cryptography/new_TL_GUI.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from encryption import *
 from decryption import *
 from fileCheck import fileExist
-from tkinter import messagebox 
   
 #GLOBAL CSUF COLORS
 t_blue = '#00244E'
@@ -18,12 +18,9 @@ class tkinterApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("Titan Lock")
         self.geometry('400x400+500+500')
-        self.minsize(500,420)
+        self.minsize(620,420)
         self.resizable(True, True)
         
-        
-        
-       
         # creating a container
         container = tk.Frame(self)
         
@@ -31,8 +28,7 @@ class tkinterApp(tk.Tk):
         container.configure(bg=t_blue)
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
-        
-       
+    
         # initializing frames to an empty array
         self.frames = {}  
   
@@ -46,26 +42,23 @@ class tkinterApp(tk.Tk):
             # startpage, page1, page2 respectively with 
             # for loop
             self.frames[F] = frame 
-  
             frame.grid(row = 0, column = 0, sticky ="nsew")
   
         self.show_frame(StartPage)
   
-    # to display the current frame passed as
-    # parameter
+    # to display the current frame passed as parameter
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-        if cont == StartPage:
-            frame.keyEntry.delete(0, tk.END)
+        #if cont == StartPage:
+        #   frame.keyEntry.delete(0, tk.END)
             
-        elif cont == AddPage:
+        if cont == AddPage:
             frame.userEntry.delete(0, 'end')
             frame.pwEntry.delete(0, 'end')
             frame.linkEntry.delete(0, 'end')
   
 # first window frame startpage
-  
 class StartPage(tk.Frame):
     def saveSaveKey(self, keyEntry, controller):
         global masterKey
@@ -81,20 +74,19 @@ class StartPage(tk.Frame):
         #print (masterKey)
         # controller.show_frame(AddPage)
         # keyEntry.delete(0, tk.END)
-      
     
     def __init__(self, parent, controller): 
         tk.Frame.__init__(self, parent)
         self.configure(bg = t_blue)
+        
         # label of frame Layout 2
         label = ttk.Label(self, text='Titan Lock 🐘', font='Arial 36', foreground = t_orange, background = t_blue)
          
-        # putting the grid in its place by using
-        # grid
-        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+        # putting the grid in its place by using grid
+        label.grid(row = 0, column = 4, padx = 160, pady = 10) 
   
         style = ttk.Style()
-        #style.theme_use('alt')
+        style.theme_use('alt')
         style.configure('TButton', foreground = 'black', width = 15)
         style.map('TButton', background=[('active', 'white')])
         addButton = ttk.Button(self, text ="Add", command = lambda : StartPage.saveAddKey(self.keyEntry.get(), controller))
@@ -103,7 +95,7 @@ class StartPage(tk.Frame):
         # using grid
         addButton.grid(row = 1, column = 4, padx = 10, pady = 10)
         ## button to show frame 2 with text layout2
-        savedButton = ttk.Button(self, text ="Show", command = lambda : StartPage.saveSaveKey(self, self.keyEntry.get(), controller))
+        savedButton = ttk.Button(self, text ="Saved", command = lambda : StartPage.saveSaveKey(self, self.keyEntry.get(), controller))
 
         # putting the button in its place by
         # using grid
@@ -115,29 +107,28 @@ class StartPage(tk.Frame):
         self.keyEntry.grid(row = 4, column = 4)
         #keyEntry.delete(0, tk.END)
         #keyEntry.delete(len(keyEntry))
-
-        
-  
-          
-# second window frame ADD BUTTON AND ENTRY PAGE
+ 
+# Second window frame ADD BUTTON AND ENTRY PAGE
 class AddPage(tk.Frame):
     def save_file(userText, userPW, link):
         messageBox.messageboxSaved(userText, userPW, link)
 
-            
+    # Function to clear the inputted information after submitting.
+    def clear_box(self):
+        self.userEntry.delete(0, 'end')
+        self.pwEntry.delete(0, 'end')
+        self.linkEntry.delete(0, 'end')  
     
     def __init__(self, parent, controller):
-       
-        
         tk.Frame.__init__(self, parent)
         self.configure(bg = t_blue)
         label = ttk.Label(self, text='Titan Lock 🐘', font='Arial 36', foreground = t_orange, background = t_blue)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+        label.grid(row = 0, column = 4, padx = 40, pady = 10)
         # button to show frame 2 with text
         # layout2
         backButton  = ttk.Button(self, text ="Back",
-                            command = lambda : controller.show_frame(StartPage))
-        backButton.grid(row = 1, column = 1)
+                command = lambda : controller.show_frame(StartPage))
+        backButton.grid(row = 0, column = 1)
 
         #ALL ENTRIES AND SUBMIT BUTTON FOR ADD PAGE
         
@@ -150,18 +141,15 @@ class AddPage(tk.Frame):
         self.linkEntry = tk.Entry(self, width ='25')
         
         #TODO: add command to submit button
-        submitButton = ttk.Button(self, text = 'SUBMIT', command = lambda: AddPage.save_file(self.userEntry.get(), self.pwEntry.get(),self.linkEntry.get()))
+        submitButton = ttk.Button(self, text = 'SUBMIT', 
+                command = lambda: [AddPage.save_file(self.userEntry.get(), self.pwEntry.get(),self.linkEntry.get()), AddPage.clear_box(self)])
         user.grid(row = 2, column = 4)
         self.userEntry.grid(row = 3, column = 4)
         pw.grid(row = 4, column = 4)
         self.pwEntry.grid(row = 5, column = 4)
         link.grid(row = 6, column = 4)
-        self.linkEntry.grid(row = 7, column = 4)
-        
+        self.linkEntry.grid(row = 7, column = 4)    
         submitButton.grid(row= 8, column = 4, pady = 30)
-        
-        
-                    
         
 # third window frame SAVED DATA PAGE
 class SavedPage(tk.Frame): 
@@ -186,7 +174,7 @@ class SavedPage(tk.Frame):
      
         # putting the button in its place 
         # by using grid
-        backButton.grid(row = 1, column = 1, padx = 10, pady = 10)
+        backButton.grid(row = 0, column = 1, padx = 10, pady = 0)
         dataLabelList = []
         def showUserData():
             
@@ -220,10 +208,6 @@ class messageBox():
         else:
             controller.show_frame(page)
 
-
-
-  
-  
 # Driver Code
 app = tkinterApp()
 app.mainloop()
